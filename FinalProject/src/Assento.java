946281688a5fcc75a385cf8a2830b1437e0d2a6f
Pablo Vinas <pablo.araujo@alu.ufc.br>
2024-11-25 13:49:23 -0300

    setters and getters

--- FinalProject/src/Assento.java
+++ FinalProject/src/Assento.java
@@ -1,4 +1,20 @@
 public class Assento {
     int numero_assento;
     boolean ocupado;
+
+    public void setNumeroAssento(int numero_assento){
+        this.numero_assento = numero_assento;
+    }
+
+    public void setOcupado(boolean value){
+        this.ocupado = value;
+    }
+
+    public int getNumeroAssento(){
+        return this.numero_assento;
+    }
+
+    public boolean getOcupado(){
+        return this.ocupado;
+    }
 }
--- FinalProject/src/Sala.java
+++ FinalProject/src/Sala.java
@@ -5,4 +5,36 @@ public class Sala {
     int numero_assentos;
     String modo_exibicao;
     List<Sessao> sessoes;
+
+    public void setNumeroSala(int numero_sala){
+        this.numero_sala = numero_sala;
+    }
+
+    public void setNumeroAssentos(int numero_assentos){
+        this.numero_assentos = numero_assentos;
+    }
+
+    public void setModoExibicao(String modo_exibicao){
+        this.modo_exibicao = modo_exibicao;
+    }
+
+    public void setSessoes(List<Sessao> sessoes){
+        this.sessoes = sessoes;
+    }
+
+    public int getNumeroSala(){
+        return this.numero_sala;
+    }
+
+    public int getNumeroAssento(){
+        return this.numero_assentos;
+    }
+
+    public String getModoExibicao(){
+        return this.modo_exibicao;
+    }
+
+    public List<Sessao> getSessoes(){
+        return this.sessoes;
+    }
 }
