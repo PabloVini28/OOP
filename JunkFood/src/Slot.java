b8e1a0a2c29f1c6d24c0aa60db59de8bce184b24
Pablo Vinicios <pablo.araujo@alu.ufc.br>
2025-01-16 09:24:53 -0300

    feat: bulding the Slot class

--- JunkFood/src/Slot.java
+++ JunkFood/src/Slot.java
@@ -5,4 +5,39 @@ public class Slot {
     private float price;
     private int quantity;
 
+    Slot(String name, float price, int quantity){
+        setName(name);
+        setPrice(price);
+        setQuantity(quantity);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public float getPrice(){
+        return this.price;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString(){
+        
+    }
+
 }
